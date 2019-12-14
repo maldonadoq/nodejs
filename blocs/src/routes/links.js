fb90dc0e281e20facef5293c5838238fefe0f6aa
Percy <maldonadoq.percy@gmail.com>
2019-12-14 00:35:21 -0500

    links crud

--- blocs/src/routes/links.js
+++ blocs/src/routes/links.js
@@ -7,8 +7,47 @@ router.get('/add', (req, res) => {
   res.render('links/add');
 });
 
-router.post('/add', (req, res) => {
-  res.send('Received');
+router.post('/add', async (req, res) => {
+  const { title, url, description } = req.body;
+  const newlink = {
+    title,
+    url,
+    description
+  };
+
+  await db.query('insert into links set ?', [newlink]);
+  res.redirect('/links');
+});
+
+router.get('/', async (req, res) => {
+  const links = await db.query('select *from links');
+  res.render('links/list', {links})
+});
+
+router.get('/delete/:id', async (req, res) => {
+  const { id } = req.params;
+
+  await db.query('delete from links where id=?', [id]);
+  res.redirect('/links');
+});
+
+router.get('/edit/:id', async (req, res) => {
+  const { id } = req.params;
+  const links = await db.query('select * from links where id = ?', [id]);
+  res.render('links/edit', {link: links[0]});
+});
+
+router.post('/edit/:id', async (req, res) => {
+  const { id } = req.params;
+  const { title, description, url } = req.body;
+  const newlink = {
+    title,
+    url,
+    description
+  };
+
+  await db.query('update links set ? where id = ?', [newlink, id]);
+  res.redirect('/links');
 });
 
 module.exports = router;
