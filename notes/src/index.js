8934a089109a06984085346c96d4f7614d9a852d
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2020-02-21 11:38:44 -0500

    index

--- notes/src/index.js
+++ notes/src/index.js
@@ -1,4 +1,8 @@
+require('dotenv').config();
+
 const app = require('./server');
+
+require('./database');
 
 app.listen(app.get('port'), () => {
     console.log('Server listening on port', app.get('port'));
